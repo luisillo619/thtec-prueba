25373b8214ff1d8ce80ea7c5ab7931f82b6fa15b
luisillo619 <luiscarlosrangel619@gmail.com>
2024-10-02 15:53:36 -0600

    second commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';  // Cambiado de 'react-dom/client' a 'react-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Cambiar createRoot a render para React 16
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')  // Cambiado para React 16
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Para medir el rendimiento en tu app
 reportWebVitals();
